90723a02c037999f332e47d5119f22ca973a011c
Jmalak03 <jossidey@hotmail.es>
2023-10-11 20:48:46 -0600

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCPETSHOP.Models;
+using System.Diagnostics;
+
+namespace MVCPETSHOP.Controllers
+{
+    public class HomeController : Controller
+    {
+     
+      
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(LoginViewModel loginDataModel)
+        {
+            if (ModelState.IsValid)
+            {
+                // AQUÍ EL CÓDIGO DE VALIDACIÓN DEL USUARIO
+                return RedirectToAction("LoginOk");
+            }
+            else
+            {
+                return View(loginDataModel);
+            }
+        }
+
+        public ActionResult LoginOK()
+        {
+            // LA VALIDACIÓN DEL USUARIO HA SIDO CORRECTA
+            return View("Privacy");
+        }
+
+
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+
+}
